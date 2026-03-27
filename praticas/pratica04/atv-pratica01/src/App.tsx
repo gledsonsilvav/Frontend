5268edcffbba00eb6acb094a0aee18145477294e
gledsonsilvav <gledson.silva@iesb.edu.br>
2026-03-27 20:36:39 -0300

    feat: concluído layout da Home com Footer dinâmico e limpeza de código

--- praticas/pratica04/atv-pratica01/src/App.tsx
+++ praticas/pratica04/atv-pratica01/src/App.tsx
@@ -1,12 +1,12 @@
 import { Container } from './components/Container';
-import { Heading } from './components/Heading';
-import { Timer, History, PlayCircleIcon, StopCircleIcon } from 'lucide-react'; 
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
 import { CountDown } from './components/CountDown';
 import { DefaultInput } from './components/DefaultInput';
 import { Cycles } from './components/Cycles';
-import { DefaultButton } from './components/DefaultButton'; // 1. Importado
+import { DefaultButton } from './components/DefaultButton';
+import { PlayCircleIcon } from 'lucide-react';
+import { Footer } from './components/Footer'; // 1. Importado!
 
 import './styles/theme.css';
 import './styles/global.css';
@@ -14,10 +14,6 @@ import './styles/global.css';
 export function App() {
   return (
     <>
-      <Container>
-        <Heading>Ignite Timer <Timer size={24} /></Heading>
-      </Container>
-
       <Container><Logo /></Container>
       <Container><Menu /></Container>
       <Container><CountDown /></Container>
@@ -25,12 +21,11 @@ export function App() {
       <Container>
         <form className='form' action=''>
           <div className='formRow'>
-            <DefaultInput 
-              id='taskInput' 
-              type='text' 
+            <DefaultInput
               labelText='Tarefa'
-              placeholder='No que você vai trabalhar?' 
-              required 
+              id='taskInput'
+              type='text'
+              placeholder='No que você vai trabalhar?'
             />
           </div>
 
@@ -42,25 +37,16 @@ export function App() {
             <Cycles />
           </div>
 
-          {/* 2. Substituindo o botão antigo pelos novos botões dinâmicos */}
           <div className='formRow'>
-            <DefaultButton 
-              icon={<PlayCircleIcon />} 
-              color='green' 
-              type="submit" 
-            />
-            
-            <DefaultButton 
-              icon={<StopCircleIcon />} 
-              color='red' 
-              type="button" 
-            />
+            {/* Mantemos apenas o botão verde de Iniciar */}
+            <DefaultButton icon={<PlayCircleIcon />} color="green" type="submit" />
           </div>
         </form>
       </Container>
 
+      {/* 2. O Rodapé finalizando a página */}
       <Container>
-        <Heading>Histórico <History size={24} /></Heading>
+        <Footer />
       </Container>
     </>
   );
